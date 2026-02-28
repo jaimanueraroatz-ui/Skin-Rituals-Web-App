64251790ec2b3d2335af92740c1252d47834e6ef
jaimanueraroatz-ui <jaimanueraroatz@gmail.com>
2026-02-28 16:20:51 +1000

    Fix base path for GitHub Pages
    Removed Tailwind CSS plugin and updated base path.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/Habit-Tracker-Web-App/',
+  base: '/Skin-Rituals-Web-App/',
+  plugins: [react()],
 })
